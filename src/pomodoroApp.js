5598fcc13609e35b1b055383a6a5a315150ee819
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-10-31 23:52:30 +0100

    added break and secession for add and sub....

--- src/pomodoroApp.js
+++ src/pomodoroApp.js
@@ -4,5 +4,20 @@
 var app = angular.module('pomodoroApp',[]);
 app.controller("TestController", function() {
   this.test = "test";
+    
 });
+    var breaktime = 5;
+    var secessiontime = 25;
+    app.controller("BreakController",function(){
+        addbreaktime=function(){breaktime++};
+        subbreaktime=function(){breaktime--};
+        
+        });
+    app.controller("SecessionController",function(){
+        addsecessiontime = function(){secessiontime++};
+        subsecessiontime = function(){secessiontime--};
+        });
+    
+    app.controller("",function(){
+    });
 })();
