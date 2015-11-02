5a24f60461c46fe8018cd62e58e1fd936b5b6fe4
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-11-02 23:44:06 +0100

    keep up the  streak

--- src/pomodoroApp.js
+++ src/pomodoroApp.js
@@ -1,23 +1,27 @@
 
-(function(){
-
+(function () {
+'use strict';
 var app = angular.module('pomodoroApp',[]);
 app.controller("TestController", function() {
   this.test = "test";
     
 });
     var breaktime = 5;
-    var secessiontime = 25;
+    var secessiontime{
+        var time =25,
+            
+    }
     app.controller("BreakController",function(){
-        addbreaktime=function(){breaktime++};
-        subbreaktime=function(){breaktime--};
-        
+        var addbreaktime=function(){breaktime++};
+        var subbreaktime=function(){breaktime--};
         });
     app.controller("SecessionController",function(){
-        addsecessiontime = function(){secessiontime++};
-        subsecessiontime = function(){secessiontime--};
+        var addsecessiontime = function(){secessiontime++};
+        var subsecessiontime = function(){secessiontime--};
         });
     
-    app.controller("",function(){
+    app.controller("timecontrols",function(){
+//        pause
+        
     });
 })();
