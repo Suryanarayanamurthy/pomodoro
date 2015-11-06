16dca0f900240a9443d4c0a6ed4dc6ef8f2c5c65
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-11-06 22:04:38 +0100

    UI change

--- src/pomodoroApp.js
+++ src/pomodoroApp.js
@@ -2,14 +2,14 @@
 (function () {
 'use strict';
 var app = angular.module('pomodoroApp',[]);
-app.controller("TestController", function() {
+app.controller("AppController", function() {
   this.test = "test";
-    
+
 });
     var breaktime = 5;
     var secessiontime{
         var time =25,
-            
+
     }
     app.controller("BreakController",function(){
         var addbreaktime=function(){breaktime++};
@@ -19,9 +19,9 @@ app.controller("TestController", function() {
         var addsecessiontime = function(){secessiontime++};
         var subsecessiontime = function(){secessiontime--};
         });
-    
+
     app.controller("timecontrols",function(){
 //        pause
-        
+
     });
 })();
