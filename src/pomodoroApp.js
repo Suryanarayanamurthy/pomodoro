544b401d15b48b82d228d6d50f22a5a2c8bf30f8
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2015-11-08 03:26:21 +0100

    cant belive i broken the freken streak

--- src/pomodoroApp.js
+++ src/pomodoroApp.js
@@ -1,8 +1,20 @@
-@import "bourbon";
 
-@import url(http://fonts.googleapis.com/css?family=Pacifico|Open+Sans:300);
 
 var app = angular.module('pomoapp',[]);
+app.controller("AppController",function(){
+
+    function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return (
+      (h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s
+    ); 
+  }
+    $scope
+    
+});
 
 (function () {
 'use strict';
